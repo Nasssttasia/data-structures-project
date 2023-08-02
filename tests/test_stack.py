5f91c12667f5e76398a081904bdc21f37ae32d2d
Anastasia Chernysheva <nastya770098@gmail.com>
2023-08-03 01:01:31 +0300

    hw2

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+import src
+from src.stack import Stack
+
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.stack = Stack()
+
+    def push(self):
+        self.assertEqual(self.stack.push('data1'), 'data1')
+
+    def pop(self):
+        self.assertEqual(self.pop(), None)
