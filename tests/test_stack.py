80b33afc082de5b50cd97d1b20b1cecbeeb234d1
Anastasia Chernysheva <nastya770098@gmail.com>
2023-08-05 01:08:42 +0300

    hw3

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,19 +1,50 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
 import unittest
 import src
-from src.stack import Stack
+from src.stack import Stack, Node
 
 
-class TestMain(unittest.TestCase):
+class TestNode(unittest.TestCase):
+    def test_node(self):
+        node = Node(1, None)
+        self.assertEqual(node.data, 1)
+        self.assertEqual(node.next_node, None)
 
-    def setUp(self) -> None:
-        self.stack = Stack()
+    def test_node_next_node(self):
+        node1 = Node(1, None)
+        node2 = Node(2, node1)
+        self.assertEqual(node2.next_node, node1)
+        self.assertEqual(node2.next_node.data, 1)
 
-    def push(self):
-        self.assertEqual(self.stack.push('data1'), 'data1')
 
-    def pop(self):
-        self.assertEqual(self.stack.pop(), None)
+class TestStack(unittest.TestCase):
+    def test_stack_empty(self):
+        stack = Stack()
+        self.assertEqual(stack.top, None)
 
-    def str(self):
-        self.assertEqual(self.__str__(), 'data1')
+    def test_stack_push1(self):
+        stack = Stack()
+        stack.push('data1')
+        self.assertEqual(stack.top.data, 'data1')
+
+    def test_stack_push2(self):
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        self.assertEqual(stack.top.data, 'data2')
+        self.assertEqual(stack.top.next_node.data, 'data1')
+
+    def test_stack_pop1(self):
+        stack = Stack()
+        stack.push('data1')
+        data = stack.pop()
+        self.assertEqual(stack.top, None)
+        self.assertEqual(data, 'data1')
+
+    def test_stack_pop2(self):
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        data = stack.pop()
+        self.assertEqual(stack.top.data, 'data1')
+        self.assertEqual(data, 'data2')
